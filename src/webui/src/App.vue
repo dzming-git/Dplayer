871c41950580494c842c884292d877cd6680b29a
dzming-git <dzm_work@163.com>
2026-07-23 22:35:19 +0800

    refactor(nav): 将"合集"入口从左上角移到导航栏「标签」与「搜索栏」之间

--- src/webui/src/App.vue
+++ src/webui/src/App.vue
@@ -129,6 +129,7 @@ const handleSubmitSuggestion = async () => {
       <div class="nav-left">
         <RouterLink to="/" class="logo">DPlayer</RouterLink>
         <RouterLink to="/tags" class="nav-link">标签</RouterLink>
+        <RouterLink to="/collections" class="nav-link" title="合集">合集</RouterLink>
         <div class="nav-search">
           <input
             v-model="searchText"
@@ -165,12 +166,6 @@ const handleSubmitSuggestion = async () => {
               <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
             </svg>
             <span>收藏</span>
-          </RouterLink>
-          <RouterLink to="/collections" class="nav-link nav-icon-link" title="合集">
-            <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-              <path d="M22 16V4c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2zm-11-4 2.03 2.71L16 11l4 5H8l3-4zM2 6v14c0 1.1.9 2 2 2h14v-2H4V6H2z"/>
-            </svg>
-            <span>合集</span>
           </RouterLink>
           <RouterLink to="/history" class="nav-link nav-icon-link" title="历史">
             <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
